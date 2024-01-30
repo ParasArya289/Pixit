c2bc193a3a4c422d94ebea2dbafc807107847702
Paras Arya <parasarya289@gmail.com>
2024-01-30 22:37:30 +0530

    feat: setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import { Home } from "./pages/home/Home";
 
 export default function App() {
   //add framer motion for page transition
   return (
     <div className="App">
-      <h1>Pixit</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
-}
+} 
--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App.2";
 import { Provider } from "react-redux";
-import { store } from "./src/services/state/store";
+import App from "./App";
+import store from "./services/state/store";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
+  </React.StrictMode>
 );
