ab6c2eba108c8b6b6a81ae80ab14e0a67a81d184
Paras Arya <parasarya289@gmail.com>
2024-01-31 23:59:58 +0530

    feat: redux store

--- src/services/state/store.js
+++ src/services/state/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { backgroundReducer } from "./backgroundSlice";
+import { searchResultReducer } from "./SearchResultSlice";
 
 export default configureStore({
   reducer: {
-    background: "",
-    search: "",
+    background: backgroundReducer,
+    searchResult: searchResultReducer,
   },
 });
