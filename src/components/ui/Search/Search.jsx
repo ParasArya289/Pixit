b186751f3d83d0405d0e9eb3bbda74a2731f7477
Paras Arya <parasarya289@gmail.com>
2024-01-31 23:51:12 +0530

    feat: search functionality

--- src/components/ui/Search/Search.jsx
+++ src/components/ui/Search/Search.jsx
@@ -1,13 +1,30 @@
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
+import { animate, motion } from "framer-motion";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  changeSearchQuery,
+  fetchSearchResult,
+} from "../../../services/state/SearchResultSlice";
 import "./Search.css";
 const Search = () => {
+  const [inputValue, setInputValue] = useState("");
+  const dispatch = useDispatch();
+  const { searchQuery } = useSelector((state) => state.searchResult);
   const navigate = useNavigate();
+  const location = useLocation();
+  const changeHandler = (e) => {
+    dispatch(changeSearchQuery(e.target.value));
+  };
   const handleSubmit = (e) => {
     e.preventDefault();
-    navigate("/search");
+    if (location.pathname !== "search") {
+      navigate("/search");
+    }
+    dispatch(fetchSearchResult(searchQuery));
   };
   return (
-    <div className="search">
+    <motion.div className="search">
       <svg
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 512 512"
@@ -17,10 +34,15 @@ const Search = () => {
       </svg>
       <div className="vr" />
       <form onSubmit={handleSubmit}>
-        <input type="text" placeholder="Search" />
+        <input
+          onChange={changeHandler}
+          type="text"
+          placeholder="Search"
+          value={searchQuery}
+        />
         <button type="submit">GO</button>
       </form>
-    </div>
+    </motion.div>
   );
 };
 
