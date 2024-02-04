379c6d7a89171a85dbcd075fb89281fb303cd4fb
Paras Arya <parasarya289@gmail.com>
2024-02-04 23:00:21 +0530

    log

--- src/components/ui/Drawer/ImageDrawer.jsx
+++ src/components/ui/Drawer/ImageDrawer.jsx
@@ -1,6 +1,7 @@
 import { Drawer } from "vaul";
 
 export const ImageDrawer = ({ children }) => {
+  //fix import issues and css variable issue
   return (
     <Drawer.Root>
       <Drawer.Trigger>{children}</Drawer.Trigger>
